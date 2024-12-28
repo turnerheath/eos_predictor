43228739c91607c9909aa79e12dc4749addbffd3
turnerheath <turnerheath00@gmail.com>
2024-12-28 16:35:32 -0500

    Update windowtest.py

--- windowtest.py
+++ windowtest.py
@@ -6,12 +6,12 @@ HONEYDEW = '#f0fff0'
 class App():
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry('350x200')
-        self.root.title('Text App')
-        self.mainframe = tk.Frame(self.root, background=HONEYDEW)
+        self.root.geometry('700x400')
+        self.root.title('EOS Coefficient Predictor')
+        self.mainframe = tk.Frame(self.root, background='#B00B13')
         self.mainframe.pack(fill='both', expand=True)
 
-        self.text = ttk.Label(self.mainframe, text='Hello World', background=HONEYDEW, font=("Times New Roman", 30))
+        self.text = ttk.Label(self.mainframe, text='Hello World', background='#B00B13', font=("Times New Roman", 30))
         self.text.grid(row=0, column=0)
 
         self.set_text_field = ttk.Entry(self.mainframe)
@@ -19,7 +19,7 @@ class App():
         set_text_button = ttk.Button(self.mainframe, text='Set Text', command=self.set_text)
         set_text_button.grid(row=1, column=1, pady=10)
 
-        color_options = ['red', 'blue', 'green', 'black']
+        color_options = ['red', 'blue', 'green', 'black', 'honeydew']
         self.set_color_field = ttk.Combobox(self.mainframe, values=color_options)
         self.set_color_field.grid(row=2, column=0, sticky='NWES', pady=0)
         set_color_button = ttk.Button(self.mainframe, text='Set Color', command=self.set_color)
